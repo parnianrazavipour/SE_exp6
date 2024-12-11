1c93789b0962df05182273d7f1ae11d3bc60c03b
Parnian <razaviparnian81@yahoo.com>
2024-12-11 20:08:51 +0330

    removed Database folder

--- database/ init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS microservices;
-USE microservices;
-
-CREATE TABLE IF NOT EXISTS records (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    value VARCHAR(255) NOT NULL
-);
